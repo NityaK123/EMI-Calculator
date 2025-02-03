2fb5a4a33f350568d6cc0b257040c87919f19054
Nityanand Gupta <54433920+NityaK123@users.noreply.github.com>
2025-02-03 07:16:29 +0000

    test folder and file created

---
+++ src/__test__/functionalTest/emiCalculator.test.js
@@ -0,0 +1,18 @@
+import {screen,render} from '@testing-library/react'
+import { calculateEmi } from '../../utils/calculateEmi'
+
+
+describe("",()=>{ 
+
+    const mockData = {
+        totalCost:710000,
+        downPaymentRate:0,
+        interestRate:8.8,
+        tenure:60
+    }
+     
+    test("Test case 1",()=>{
+        const emiReceived = calculateEmi(mockData)
+        expect(emiReceived.emi).toBe(14670)
+    })
+})
